bd3bc337d9970633880cad270e4c5ce723e711ed
Nicola Silvestri <ns.nicolasilvestri@libero.it>
2022-05-03 17:14:36 +0200

    first setup

---
+++ src/MusicLibraryApi.Database/dbo/Tables/RecordLabels.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RecordLabels] (
+    [Id]               UNIQUEIDENTIFIER NOT NULL,
+    [Name]             NVARCHAR (50)    NOT NULL,
+    [FoundationDate]   DATE             NOT NULL,
+    [CreationDate]     DATETIME         NOT NULL,
+    [LastModifiedDate] DATETIME         NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- src/MusicLibraryApi/Program.cs
+++ src/MusicLibraryApi/Program.cs
@@ -1,25 +1,34 @@
+using Hellang.Middleware.ProblemDetails;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Serilog;
+using System.Text.Json.Serialization;
+using TinyHelpers.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+{
+    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+});
+builder.Services.AddProblemDetails();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+        options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen()
+    .AddFluentValidationRulesToSwagger(options =>
+{
+    options.SetNotNullableIfMinLengthGreaterThenZero = true;
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
+app.UseProblemDetails();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
